d1b61a1751425d778f93e934bd9deb7be732cc41
Young <young20050727@gmail.com>
2024-03-22 07:56:22 +0000

    Update 3.cpp

--- 20240322/3.cpp
+++ 20240322/3.cpp
@@ -3,14 +3,14 @@
 int fib(int n) {
   if (n == 0) return 0;
   if (n == 1) return 1;
-  int n1, n2, n3;
+  int n1 = 0, n2 = 1, n3 = 0;
   for (int i=2; i<n; ++i) {    
     n3=n1+n2;   
     n1=n2;
     n2=n3;
   }
 
-  return (fib(n-1)) + (fib(n-2));
+  return n1 + n2;
 }
 
 int main() {
