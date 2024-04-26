b028f346cbcb343d8d51d76951d8f3eb862de36d
young <young20050727@gmail.com>
2024-04-26 15:29:59 +0800

    20240426-2

---
+++ 20240426/2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+
+int* myMax(int *a, int *b) {
+    if (*a > *b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+int main() {
+    int a = 10;
+    int b = 20;
+    std::cout << "before:\n";
+    std::cout << "a=" << a << "\tb=" << b << std::endl;
+    int *ptr = myMax(&a, &b);
+    std::cout << *ptr << std::endl;
+    return 0;
+}
