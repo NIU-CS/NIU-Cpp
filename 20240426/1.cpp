ad18934efced14578947100033fbdf74d80a01f7
young <young20050727@gmail.com>
2024-04-26 15:05:56 +0800

    20240426

---
+++ 20240426/1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    return;
+}
+
+int main() {
+    int a = 10;
+    int b = 20;
+    std::cout << "before:\n";
+    std::cout << "a=" << a << "\tb=" << b << std::endl;
+    swap(&a, &b);
+    std::cout << "after:\n";
+    std::cout << "a=" << a << "\tb=" << b << std::endl;
+    return 0;
+}
